ce3b0b42d8fbcc289a16838ffeedd3b72dbb738f
marcin <marr1@tlen.pl>
2020-11-09 09:31:12 +0100

    adding script for 1st test. Webdriver refactored to wd added new file 3a

--- test_add_group3a.py
+++ test_add_group3a.py
@@ -1,23 +1,16 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-import unittest, time, re
+import unittest
+
 
 class TestAddGroup3(unittest.TestCase):
     def setUp(self):
-        self.wd = .Firefowebdriverx()
-        binary = FirefoxBinary('C:\Program Files\Mozilla Firefox\Firefox.exe')
+        self.wd = webdriver.Firefox()
+        binary = FirefoxBinary("C:\Program Files\Mozilla Firefox\Firefox.exe")
         self.wd = webdriver.Firefox(firefox_binary=binary)
         self.wd.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-    
+
     def test_add_group3(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
@@ -62,31 +55,11 @@ class TestAddGroup3(unittest.TestCase):
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_link_text("Logout").click()
-    
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.wd.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
+
+
     def tearDown(self):
         self.wd.quit()
-        self.assertEqual([], self.verificationErrors)
+
 
 if __name__ == "__main__":
     unittest.main()
