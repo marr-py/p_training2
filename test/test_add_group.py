87c126ba7a59ccd1571969091f4e3dba847fd26a
marcin <marr1@tlen.pl>
2020-12-02 21:48:39 +0100

    Task 11. Checking list of groups.+ checking list of contacts. + issue with sorting groups

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-
+from sys import maxsize
 
 def test_add_group3b(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(groupname="Grupa nr 17", grouphead1="naglowek nr 11", groupfooter1="stopka nr 11"))
+    group = Group(groupname="Grupa nr 19", grouphead1="naglowek nr 19", groupfooter1="stopka nr 19")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
+    assert len(old_groups) + 1 == len(old_groups)
+    old_groups.append(group)
+        def id_or_max(gr):
+            if gr.id:
+                return gr.id
+            else:
+                return maxsize
+    assert sorted(old_groups, key=id_or_max) == sorted(new_groups, key=id_or_max)
 
 
 def test_add_empty_group(app):
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -7,5 +7,6 @@ def test_delete_first_group(app):
         app.group.create(Group(groupname="test", grouphead1="test", groupfooter1="test"))
     old_groups = app.group.get_group_list()
     app.group.delete_first_group()
+    assert len(old_groups) - 1 == app.group.count()
     new_groups = app.group.get_group_list()
     assert len(old_groups) - 1 == len(new_groups)
